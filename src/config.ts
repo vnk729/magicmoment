231c8e884fe7ade6f23664a5f5f880d4e4d623c5
vnk <vnk729@gmail.com>
2026-02-14 13:41:39 +0700

    localize invitation

--- src/config.ts
+++ src/config.ts
@@ -2,20 +2,20 @@ import { assets } from "$app/paths";
 
 export const config = {
 	mainPage: {
-		heading: "Hello!",
-		invitation: "Will you go on a date with me this week? 👉👈🥺",
-		yesText: "Yes",
-		noText: "No",
+		heading: "Привет, Настя!",
+		invitation: "Пойдёшь со мной на свидание на этой неделе? 👉👈🥺",
+		yesText: "Да",
+		noText: "Нет",
 		topImage: `${assets}/Assets/Top_picture/Cute_pic2.jpg`,
 		leftHeart: `${assets}/Assets/Hearts/heart_3d.gif`,
 		rightHeart: `${assets}/Assets/Hearts/heart_3d.gif`,
 	},
 	noPage: {
-		message: '"No" is not really an option😁',
+		message: '"Нет" – это вообще не вариант 😁',
 		image: `${assets}/Assets/No_picture/kermit-shooting.gif`,
 	},
 	yesPage: {
-		message: "See you this week 😄",
+		message: "Увидимся на этой неделе 😉",
 		image: `${assets}/Assets/Yes_picture/Yes.gif`,
 	},
 };
